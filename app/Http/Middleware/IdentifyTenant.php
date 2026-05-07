f01a1c8661105ca9d84c9f87d12567cda048d171
Sakib <nsakib.miraz@gmail.com>
2026-05-07 14:44:58 +0600

    ,,

--- app/Http/Middleware/IdentifyTenant.php
+++ app/Http/Middleware/IdentifyTenant.php
@@ -10,24 +10,17 @@ class IdentifyTenant
 {
     public function handle(Request $request, Closure $next)
     {
-        $host = preg_replace('/^www\./', '', $request->getHost());
+        $host       = preg_replace('/^www\./', '', $request->getHost());
         $mainDomain = preg_replace('/^www\./', '', parse_url(config('app.url'), PHP_URL_HOST));
 
-        if (
-            $host === $mainDomain ||
-            filter_var($host, FILTER_VALIDATE_IP) ||
-            $host === 'localhost'
-        ) {
+        if ($host === $mainDomain || filter_var($host, FILTER_VALIDATE_IP)) {
             return $next($request);
         }
 
-        $tenant = Tenant::where('domain', $host)
-            ->where('status', 1)
-            ->first();
+        $tenant = Tenant::where('domain', $host)->where('status', 1)->first();
 
         if ($tenant) {
-
-            if ($request->is('login') || $request->is('register')) {
+            if ($request->is('login')) {
                 abort(404);
             }
 
